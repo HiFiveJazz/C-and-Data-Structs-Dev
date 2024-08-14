4a71db1ab70d67b026eaaeafeddc82cda1624110
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-14 00:44:50 -0700

    Testing out some C code

---
+++ Challenge #1/double.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  double x;
+  scanf("%lf", &x);
+  printf("Value: %lf\n", x);
+}
+
+/* int main(){ */
+/*   float x; */
+/*   scanf("%f", &x); */
+/*   printf("Value: %f\n", x); */
+/* } */
---
+++ Challenge #1/name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+  char str[100];
+  printf("Input your name: \n");
+  scanf("%s", str);
+  printf("Your name is: %s\n", str);
+  return 0;
+} 
