e4bd9ce985ac14c41229a7d750a7ed9a543ef65e
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-13 22:07:03 -0700

    Test

---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int myFavoriteNumber = 0;
+  printf("Please enter your favorite number: ");
+  scanf("%d",&myFavoriteNumber);
+  printf("Your favorite number is: %d\n", myFavoriteNumber);
+  return 1;
+}
