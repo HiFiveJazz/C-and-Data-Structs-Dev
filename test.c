6bff674977b7f585ae3bbd81e2862427ca07af21
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-15 22:58:21 -0700

    cleanup

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int myFavoriteNumber = 0;
-  printf("Please enter your favorite number: ");
-  scanf("%d",&myFavoriteNumber);
-  printf("Your favorite number is: %d\n", myFavoriteNumber);
-  return 1;
-}
