9b68d6985e38b39d8b24221186a0322d6a55556f
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-15 22:55:31 -0700

    Finish Section 5

--- Printing/test.c
+++ Printing/test.c
@@ -3,16 +3,14 @@
 int main(){
   int integerVar = 100;
   float floatingVar = 331.79;
-  float doubleVar = 8.44e+11;
+  double doubleVar = 8.44e+11;
   char charVar = 'W';
   _Bool boolVar = 0;
 
   printf("integerVar = %i\n", integerVar);
   printf("floatingVar = %f\n", floatingVar);
+  printf("floatingVar to 2 precision = %.2f\n", floatingVar);
   printf("doubleVar = %e\n", doubleVar);
   printf("charVar = %c\n", charVar);
   printf("boolVar = %\n", boolVar);
-  /* prinf("Var = %\n", ) */
-  /* prinf("Var = %\n", ) */
-  /* prinf("Var = %\n", ) */
 }
---
+++ Section_6/rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  double height = 10;
+  double width = 10;
+  double area = height * width;
+  double perimeter = 2 * (height + width);
+  printf("Area: %.1f\n", area);
+  printf("Perimeter: %.1f\n", perimeter);
+}
