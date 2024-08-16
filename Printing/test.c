8440ab869ab7bba82319b3772016dc1244bb13ab
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-15 22:24:31 -0700

    printing

---
+++ Printing/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int integerVar = 100;
+  float floatingVar = 331.79;
+  float doubleVar = 8.44e+11;
+  char charVar = 'W';
+  _Bool boolVar = 0;
+
+  printf("integerVar = %i\n", integerVar);
+  printf("floatingVar = %f\n", floatingVar);
+  printf("doubleVar = %e\n", doubleVar);
+  printf("charVar = %c\n", charVar);
+  printf("boolVar = %\n", boolVar);
+  /* prinf("Var = %\n", ) */
+  /* prinf("Var = %\n", ) */
+  /* prinf("Var = %\n", ) */
+}
