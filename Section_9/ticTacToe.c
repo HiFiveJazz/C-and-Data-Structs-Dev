0f39dfc862dc120943ec421bde963b134ac18fdf
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-25 18:15:21 -0700

    tic tac toe update

--- Section_9/ticTacToe.c
+++ Section_9/ticTacToe.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-/* 1 is x, 0 is o */
-char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'}, mark;
 int choice, player;
+int checkForWin();
+int drawBoard();
+int markBoard();
 
 int main() {
-
+  drawBoard();
+  player = 1;
+  markBoard(player);
+  drawBoard();
   return 0;
 }
 
@@ -13,19 +19,45 @@ int checkForWin() {
 }
 
 int drawBoard() {
-  printf("   |   |   \n")
-  printf(" 1 | 2 | 3 \n")
-  printf("___|___|___\n")
-  printf("   |   |   \n")
-  printf(" 4 | 5 | 6 \n")
-  printf("___|___|___\n")
-  printf("   |   |   \n")
-  printf(" 7 | 8 | 9 \n")
-  printf("   |   |   \n")
-
-  printf("Player 1, enter a number. \n")
+  printf("     |     |\n");
+  printf("  %c  |  %c  |  %c\n",square[1],square[2],square[3]); // Row 1
+  printf("_____|_____|_____\n");
+  printf("     |     |\n");
+  printf("  %c  |  %c  |  %c\n",square[4],square[5],square[6]); // Row 2
+  printf("_____|_____|_____\n");
+  printf("     |     |\n");
+  printf("  %c  |  %c  |  %c\n",square[7],square[8],square[9]); // Row 3
+  printf("     |     |\n");
 }
 
-int markBoard() {
+int markBoard(int player) {
+char mark;
+int choice, out = 0;
 
+while (out == 0){  
+  out = 0;
+  printf("Player %d, please input a number: ",player);
+  scanf("%d", &choice);
+  printf("Choice value: %d\n", choice);
+  if (choice < 1 || choice > 9) {
+    printf("Invalid choice. ");
+  } 
+  else {
+    out = 1;
+    switch (player) {
+      case 1:
+        mark = 'X';
+        printf("Test X: %d\n", choice);
+        break;
+        case 2:
+        printf("Test Y: %d\n", choice);
+        mark = 'O';
+        break;
+      default:
+        printf("Error: Player is not defined as 1 or 2\n");
+      square[1] = mark;
+    }
+  }
 }
+}
+
