f0aea243cecf237f16e10a2db0a4e836cfe9c92a
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-25 17:35:07 -0700

    tic tac toe

---
+++ Section_9/ticTacToe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/* 1 is x, 0 is o */
+char square[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int choice, player;
+
+int main() {
+
+  return 0;
+}
+
+int checkForWin() {
+
+}
+
+int drawBoard() {
+  printf("   |   |   \n")
+  printf(" 1 | 2 | 3 \n")
+  printf("___|___|___\n")
+  printf("   |   |   \n")
+  printf(" 4 | 5 | 6 \n")
+  printf("___|___|___\n")
+  printf("   |   |   \n")
+  printf(" 7 | 8 | 9 \n")
+  printf("   |   |   \n")
+
+  printf("Player 1, enter a number. \n")
+}
+
+int markBoard() {
+
+}
