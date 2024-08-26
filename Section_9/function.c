e8b5be0a4007b4e87e52ef93e84380fa58178ccd
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-25 17:21:36 -0700

    functionsss

--- Section_9/function.c
+++ Section_9/function.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
-void add();
+/* void add(); */
+float absoluteValue();
+int gcd(int x, int y);
 
 int main(){
-  add();
+  float a = absoluteValue(-3);
   return 0;
 }
 
-void add(){
-  printf("Hello!\n");
+int gcd(int x, int y) {
+  int temp;
+  while (y!=0){
+    temp = x % y;
+    x = y = temp;
+
+  }
+
 }
+
+float absoluteValue(float x){
+  if (x < 0) {
+    x = x * -1;
+  } 
+  return x;
+  printf("Absolute Value: %d\n",x);
+  return x;
+}
+
+/* float squareRoot(float y){ */
+/*   float abs = absoluteValue(y); */
+/**/
+/*   return x; */
+/*   printf("Absolute Value: %d\n",x); */
+/*   return x; */
+/* } */
